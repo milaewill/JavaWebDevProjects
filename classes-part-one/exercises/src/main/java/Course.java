1796f8aca8ff6228152c894b7e4c692149315390
milaewill <milaewilliams@gmail.com>
2023-12-08 10:44:19 -0600

    class01studio

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Course {
+
+        private String topic;
+        private Teacher instructor;
+        private ArrayList<Student> enrolledStudents;
+
+        public Course(String topic, Teacher instructor) {
+            this.topic = topic;
+            this.instructor = instructor;
+            this.enrolledStudents = new ArrayList<>();
+        }
+
+        public void enrollStudent(Student student) {
+        enrolledStudents.add(student);
+        }
+
+    }
+
+
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,12 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+
+        Student student = new Student("Mila", 62894, 1, 4.0);
+        Teacher teacher = new Teacher("Gerard", "Darris", "LC101",5);
+        Course javaCourse = new Course("Java Programming", teacher);
+
+        javaCourse.enrollStudent(student);
+
     }
 }
