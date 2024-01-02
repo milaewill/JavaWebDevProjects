5381a1acffd02c87cd011855ddf6f6f11d5fdd62
milaewill <milaewilliams@gmail.com>
2024-01-02 15:18:46 -0600

    studio

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,52 @@
 package org.launchcode;
 
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+
+        //menu items
+        MenuItem item1 = new MenuItem("toasted ravioli", "a classic stl dish", 14, "appetizer");
+        MenuItem item2 = new MenuItem("spaghetti and meatballs", "fresh pasta", 23, "entree");
+        MenuItem item3 = new MenuItem("tiramisu", "creamy and delicious", 12, "dessert");
+
+        //print item
+        System.out.println(item1);
+
+        //create menu
+        Menu menu = new Menu();
+
+
+        //add items to the menu
+        menu.addItem(item1);
+        menu.addItem(item2);
+        menu.addItem(item3);
+
+        //print menu
+        System.out.println(menu);
+
+        //remove item
+        menu.removeItem(item3);
+        System.out.println(menu);
+
+        //equals method test
+        System.out.println(item1.equals(item2));
+
+
+        //attempting duplicate item
+
+        MenuItem item4 = new MenuItem("Buratta", "creamy cheese ball", 17,"appetizer");
+
+        System.out.println(item3.equals(item4));
+
+        menu.addItem(item4);
+        System.out.println(menu);
+
+        menu.addItem(item1);
+        System.out.println(menu);
+
+
     }
+
 }
--- classes-part-2/studio/src/main/java/org/launchcode/Menu.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Menu.java
@@ -1,32 +1,70 @@
 package org.launchcode;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
+
 
 public class Menu {
-    private Date lastUpdated;
-    private ArrayList<MenuItem> items;
 
-    public Menu(Date d, ArrayList<MenuItem> i) {
-        this.lastUpdated = d;
-        this.items = i;
+    private ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private LocalDate lastUpdated;
+
+    //getters and setters
+
+    public ArrayList<MenuItem> getMenuItems() {
+            return menuItems;
     }
 
-    public void setLastUpdated(Date lastUpdated) {
-        this.lastUpdated = lastUpdated;
+    @Override
+    public String toString() {
+        StringBuilder appetizer = new StringBuilder();
+        for (MenuItem item: menuItems) {
+            if (item.getCategory().equals("appetizer")) {
+                appetizer.append("\n").append(item.toString()).append("\n");
+            }
+        } StringBuilder entree = new StringBuilder();
+        for (MenuItem item: menuItems) {
+            if (item.getCategory().equals("entree")) {
+                entree.append("\n").append(item.toString()).append("\n");
+            }
+        }StringBuilder dessert = new StringBuilder();
+        for (MenuItem item: menuItems) {
+            if (item.getCategory().equals("dessert")) {
+                dessert.append("\n").append(item.toString()).append("\n");
+            }
+
+        }
+        return "\nItaliana\n" +
+                "APPETIZERS" + appetizer.toString() + "\n" +
+                "ENTREES" + entree.toString() + "\n" +
+                "DESSERTS" + dessert.toString() + "\n";
+
     }
 
-    public void setItems(ArrayList<MenuItem> items) {
-        this.items = items;
+    void addItem(MenuItem newItem) {
+        String message = "Item is already on the menu.";
+        if (menuItems.contains(newItem)) {
+            System.out.println(message);
+            return;
+        }
+        for (MenuItem item : menuItems) {
+            if(item.equals(newItem)) {
+                System.out.println(message);
+                return;
+
+            }
+        }
+
+        menuItems.add(newItem);
+        lastUpdated = LocalDate.now();
     }
 
-    public Date getLastUpdated() {
-        return lastUpdated;
+    void removeItem(MenuItem item) {
+        menuItems.remove(item);
+        lastUpdated = LocalDate.now();
     }
 
-    public ArrayList<MenuItem> getItems() {
-        return items;
-    }
+
 }
 
 
